be36530f186a3a33adb978cc2d66f38812b9d4a7
Bienn <biennviquiera@gmail.com>
2022-06-27 13:54:38 -0700

    created home feed cell file

---
+++ Parsetagram/Views/HomeFeedCell.m
@@ -0,0 +1,23 @@
+//
+//  HomeFeedCell.m
+//  Parsetagram
+//
+//  Created by Bienn Viquiera on 6/27/22.
+//
+
+#import "HomeFeedCell.h"
+
+@implementation HomeFeedCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
