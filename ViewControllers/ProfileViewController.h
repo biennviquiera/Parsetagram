2a1ac5592b42cfe230ab236f48182c4cbc22b21b
Bienn <biennviquiera@gmail.com>
2022-06-27 14:10:15 -0700

    configured all view controllers' custom classes

---
+++ ViewControllers/ProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.h
+//  Parsetagram
+//
+//  Created by Bienn Viquiera on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
