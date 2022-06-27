5bb08c0ca3be4b398a487c79ad831605509af7f8
Bienn <biennviquiera@gmail.com>
2022-06-27 12:58:02 -0700

    Initial commit

---
+++ Parsetagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Parsetagram
+//
+//  Created by Bienn Viquiera on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Parsetagram/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Parsetagram
+//
+//  Created by Bienn Viquiera on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
